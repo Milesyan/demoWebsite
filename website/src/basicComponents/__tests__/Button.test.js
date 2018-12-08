c01b5c7bf8169e3fefb6eb67c7429a7f157263e6
miles <miles@glowing.com>
2018-12-08 21:17:54 +0800

    eject create reate app and setup jest automation

--- website/src/basicComponents/__tests__/Button.test.js
+++ website/src/basicComponents/__tests__/Button.test.js
@@ -16,11 +16,24 @@ const setup = overrideProps => {
   return { wrapper, props };
 }
 
-describe('Button Snapshot', () => {
+describe('Button snapshots', () => {
 
   it('should match snapshot when button is not disabled', () => {
     const { wrapper } = setup();
     expect(wrapper).toMatchSnapshot();
+  });
+  
+  it('should match snapshot when button is disabled', () => {
+    const { wrapper } = setup({isDisabled: true});
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
+
+describe('Button functions', () => {
+
+  it('should call onClick function when the button is not disabled.', () => {
+    const { wrapper } = setup();
   });
   
 });
--- website/src/testConfig/setup.js
+++ website/src/testConfig/setup.js
@@ -4,6 +4,12 @@ import Adapter from 'enzyme-adapter-react-16';
 
 import { shallow } from 'enzyme';
 
+if (typeof window !== 'undefined') {
+  // fetch() polyfill for making API calls.
+  require('whatwg-fetch');
+}
+
 global.shallow = shallow;
+global.React = React;
 
 configure({ adapter: new Adapter() })
